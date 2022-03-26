75183d3294d133f80322863407a799db260a78f2
photinospeter@gmail.com <photinospeter@gmail.com>
2022-03-26 05:25:38 +0000

    array vectors

--- models/detect/array_vectors.sql
+++ models/detect/array_vectors.sql
@@ -4,8 +4,8 @@ select
   session_id 
   , source_ip
   , array_agg(user_event order by user_event asc) as event_name_vector
-  , array_agg(prop_interaction order by user_event asc) as prop_vector
-  , array_agg(n order by user_event asc) as v_vector
+  , array_agg(cast(prop_interaction as float) order by user_event asc) as prop_vector
+  , array_agg(n order by user_event asc) as n_vector
 from {{ ref('interaction_vectors') }}
 where true
 group by 1,2
